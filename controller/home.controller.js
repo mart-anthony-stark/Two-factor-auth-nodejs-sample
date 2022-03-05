c80481ef4fc79683f47f7b3276e4bdffdc1d044e
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-05 23:33:46 +0800

    add response

--- controller/home.controller.js
+++ controller/home.controller.js
@@ -17,7 +17,17 @@ module.exports = {
         template: "Your verification code is %token.",
       },
       (err, response) => {
-        if (err) console.log(err);
+        if (err) {
+          console.log(err);
+          res.render("step1", {
+            error: err.errors[0].description,
+          });
+        } else {
+          console.log(response);
+          res.render("step2", {
+            id: response.id,
+          });
+        }
       }
     );
   },
