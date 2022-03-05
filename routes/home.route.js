d26bcad44a3cf26e5acfd6083f7067b791917f69
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-05 23:38:43 +0800

    fixed rout

--- routes/home.route.js
+++ routes/home.route.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const homeController = require("../controller/home.controller");
 
 router.get("/", homeController.getHome);
-router.post("/step2", homeController.postVerify);
+router.post("/step2", homeController.postSubmitNumber);
+router.post("/step3", homeController.postVerify);
 
 module.exports = router;
